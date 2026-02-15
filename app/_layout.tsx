0a392594424f7a5b1b14d39a135173a5aad83c7e
DelwinPrakash <delwinprakash499@gmail.com>
2026-02-15 13:40:31 +0530

    feat: added conection providers and authorization providers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
+import '../global.css';
+import 'react-native-get-random-values';
+import 'react-native-reanimated';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider } from '../context/AuthContext';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ConnectionProvider } from '@/components/providers/ConnectionProvider';
+import { AuthorizationProvider } from '@/components/providers/AuthorizationProvider';
+import { RPC_ENDPOINT } from '@/components/providers/ConnectionProvider';
+import {clusterApiUrl} from '@solana/web3.js';
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,12 +22,21 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ConnectionProvider config={{commitment: 'processed'}} endpoint={clusterApiUrl(RPC_ENDPOINT)}>
+        <AuthorizationProvider>
+          <AuthProvider>
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+                <Stack.Screen name="pay/[username]" options={{ presentation: 'card', title: 'Pay' }} />
+              </Stack>
+              <StatusBar style="auto" />
+            </ThemeProvider>
+          </AuthProvider>
+        </AuthorizationProvider>
+      </ConnectionProvider>
+    </SafeAreaProvider>
   );
 }
